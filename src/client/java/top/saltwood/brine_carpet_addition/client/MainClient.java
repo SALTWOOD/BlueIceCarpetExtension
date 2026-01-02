880d13c427883a1bd1a31abf4c509d2b8d50c479
SaltWood_233 <105980161+SALTWOOD@users.noreply.github.com>
2026-01-02 23:51:37 +0900

    init repository

---
+++ src/client/java/top/saltwood/brine_carpet_addition/client/MainClient.java
@@ -0,0 +1,10 @@
+package top.saltwood.brine_carpet_addition.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MainClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/top/saltwood/brine_carpet_addition/Main.java
@@ -0,0 +1,10 @@
+package top.saltwood.brine_carpet_addition;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
